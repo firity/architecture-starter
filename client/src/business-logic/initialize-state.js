7478bbfe30b6bf9e55cfc33ced3935116f563cb9
Rosa Moran <78501106+RosaMoran@users.noreply.github.com>
2021-09-09 10:03:29 +0200

    Delete client/src/business-logic directory
    deleting this doc cuz it's not about our homework

--- client/src/business-logic/initialize-state.js
+++
@@ -1,7 +0,0 @@
-// keep this file!  it works, and you will need it
-
-import { load } from '../data-access/load.js';
-
-export const initializeState = async () => {
-  await load('../../../data/numbers.json', import.meta);
-};
